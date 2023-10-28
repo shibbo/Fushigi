04f0fc328803957b875ef090489cacf84f112c63
shibbo <supershibbo12@gmail.com>
2023-10-28 11:44:49 -0400

    reduce the amount of nesting in ParamDB

--- Fushigi/param/ParamDB.cs
+++ Fushigi/param/ParamDB.cs
@@ -70,138 +70,142 @@ namespace Fushigi.param
                 SARC.SARC sarc = new SARC.SARC(new MemoryStream(fileBytes));
 
                 /* /Component/Blackboard/BlackboardParamTable is where all of the actor-specific parameters live */
-                if (sarc.DirectoryExists("Component/Blackboard/BlackboardParamTable"))
+                if (!sarc.DirectoryExists("Component/Blackboard/BlackboardParamTable"))
                 {
-                    /* grab every file in this directory, should be all BYMLs */
-                    string[] filesInDir = sarc.GetFiles("Component/Blackboard/BlackboardParamTable");
+                    continue;
+                }
 
-                    foreach(string paramFile in filesInDir)
+                /* grab every file in this directory, should be all BYMLs */
+                string[] filesInDir = sarc.GetFiles("Component/Blackboard/BlackboardParamTable");
+
+                foreach(string paramFile in filesInDir)
+                {
+                    /* grab our parameter file name, while wiping away the rest of the junk we don't care about */
+                    string paramFileName = Path.GetFileNameWithoutExtension(paramFile).Split(".engine")[0];
+                    /* actor parameters only need to store the paramter file name */
+                    param.Components.Add(paramFileName);
+
+                    Byml.Byml byml = new Byml.Byml(new MemoryStream(sarc.OpenFile(paramFile)));
+                    var root = (BymlHashTable)byml.Root;
+
+                    string type = "";
+
+                    // do we already have this component loaded?
+                    // if so, we skip this so we don't load the same parameters a million times
+                    if (sComponents.ContainsKey(paramFileName))
                     {
-                        /* grab our parameter file name, while wiping away the rest of the junk we don't care about */
-                        string paramFileName = Path.GetFileNameWithoutExtension(paramFile).Split(".engine")[0];
-                        /* actor parameters only need to store the paramter file name */
-                        param.Components.Add(paramFileName);
+                        continue;
+                    }
 
-                        Byml.Byml byml = new Byml.Byml(new MemoryStream(sarc.OpenFile(paramFile)));
-                        var root = (BymlHashTable)byml.Root;
+                    Component component = new Component();
+                    /* by default, parameters do not have a parent */
+                    component.Parent = "null";
+                    component.Parameters = new Dictionary<string, ComponentParam>();
 
-                        string type = "";
-
-                        // do we already have this component loaded?
-                        // if so, we skip this so we don't load the same parameters a million times
-                        if (sComponents.ContainsKey(paramFileName))
+                    /* this is where things can become a little complex
+                        * after the root node there are dictionaries that contain arrays of parameters that are a specific type (or a node that defines a parent)
+                        * here we loop through each of these nodes to sort out which type we are dealing with here */
+                    foreach (BymlHashPair pair in root.Pairs)
+                    {
+                        switch (pair.Name)
                         {
-                            continue;
+                            case "BlackboardParamS16Array":
+                                type = "S16";
+                                break;
+                            case "BlackboardParamU32Array":
+                                type = "U32";
+                                break;
+                            case "BlackboardParamS32Array":
+                                type = "S32";
+                                break;
+                            case "BlackboardParamF32Array":
+                                type = "F32";
+                                break;
+                            case "BlackboardParamF64Array":
+                                type = "F64";
+                                break;
+                            case "BlackboardParamBoolArray":
+                                type = "Bool";
+                                break;
+                            case "BlackboardParamStringArray":
+                                type = "String";
+                                break;
+                            default:
+                                break;
                         }
 
-                        Component component = new Component();
-                        /* by default, parameters do not have a parent */
-                        component.Parent = "null";
-                        component.Parameters = new Dictionary<string, ComponentParam>();
-
-                        /* this is where things can become a little complex
-                         * after the root node there are dictionaries that contain arrays of parameters that are a specific type (or a node that defines a parent)
-                         * here we loop through each of these nodes to sort out which type we are dealing with here */
-                        foreach (BymlHashPair pair in root.Pairs)
+                        /* if our current node is a BymlNode<string> and *not* a BymlArrayNode, that means we have hit a parent parameter */
+                        if (pair.Value is BymlNode<string>)
                         {
-                            switch (pair.Name)
+                            component.Parent = Path.GetFileName(((BymlNode<string>)pair.Value).Data.Split(".engine")[0]);
+                        }
+                        else
+                        {
+                            /* we are currently dealing with an array node of parameters */
+                            BymlArrayNode tbl = (BymlArrayNode)pair.Value;
+
+                            /* there are some files that contain array lengths of 0, despite the node still being written to the file */
+                            if (tbl.Length <= 0)
                             {
-                                case "BlackboardParamS16Array":
-                                    type = "S16";
-                                    break;
-                                case "BlackboardParamU32Array":
-                                    type = "U32";
-                                    break;
-                                case "BlackboardParamS32Array":
-                                    type = "S32";
-                                    break;
-                                case "BlackboardParamF32Array":
-                                    type = "F32";
-                                    break;
-                                case "BlackboardParamF64Array":
-                                    type = "F64";
-                                    break;
-                                case "BlackboardParamBoolArray":
-                                    type = "Bool";
-                                    break;
-                                case "BlackboardParamStringArray":
-                                    type = "String";
-                                    break;
-                                default:
-                                    break;
+                                continue;
                             }
 
-                            /* if our current node is a BymlNode<string> and *not* a BymlArrayNode, that means we have hit a parent parameter */
-                            if (pair.Value is BymlNode<string>)
+                            /* iterate through each node, these should be a BymlHashTable node */
+                            foreach (IBymlNode node in tbl.Array)
                             {
-                                component.Parent = Path.GetFileName(((BymlNode<string>)pair.Value).Data.Split(".engine")[0]);
-                            }
-                            else
-                            {
-                                /* we are currently dealing with an array node of parameters */
-                                BymlArrayNode tbl = (BymlArrayNode)pair.Value;
-
-                                /* there are some files that contain array lengths of 0, despite the node still being written to the file */
-                                if (tbl.Length > 0)
+                                /* this check should not fail */
+                                if (node is BymlHashTable)
                                 {
-                                    /* iterate through each node, these should be a BymlHashTable node */
-                                    foreach (IBymlNode node in tbl.Array)
+                                    BymlHashTable ht = node as BymlHashTable;
+
+                                    /* if the IsInstanceParam value is False, it means that the parameter is not used in a course context
+                                        * so, if it is False, we ignore it and move on to the next parameter as we will only read what matters
+                                        */
+                                    bool isInstParam = ((BymlNode<bool>)(ht["IsInstanceParam"])).Data;
+
+                                    if (!isInstParam)
                                     {
-                                        /* this check should not fail */
-                                        if (node is BymlHashTable)
-                                        {
-                                            BymlHashTable ht = node as BymlHashTable;
-
-                                            /* if the IsInstanceParam value is False, it means that the parameter is not used in a course context
-                                             * so, if it is False, we ignore it and move on to the next parameter as we will only read what matters
-                                             */
-                                            bool isInstParam = ((BymlNode<bool>)(ht["IsInstanceParam"])).Data;
-
-                                            if (!isInstParam)
-                                            {
-                                                continue;
-                                            }
-
-                                            ComponentParam comp = new ComponentParam();
-                                            string key = ((BymlNode<string>)(ht["BBKey"])).Data;
-                                            comp.Type = type;
-
-                                            /* we look through the type that we set earlier to assign the proper initial value */
-                                            switch (type)
-                                            {
-                                                case "U32":
-                                                    comp.InitValue = ((BymlNode<uint>)ht["InitVal"]).Data;
-                                                    break;
-                                                /* S16 and S32 are both internally still a Int32 node, but S16 has bounds checks */
-                                                case "S16":
-                                                case "S32":
-                                                    comp.InitValue = ((BymlNode<int>)ht["InitVal"]).Data;
-                                                    break;
-                                                case "F32":
-                                                    comp.InitValue = ((BymlNode<float>)ht["InitVal"]).Data;
-                                                    break;
-                                                case "F64":
-                                                    comp.InitValue = ((BymlNode<double>)ht["InitVal"]).Data;
-                                                    break;
-                                                case "Bool":
-                                                    comp.InitValue = ((BymlNode<bool>)ht["InitVal"]).Data;
-                                                    break;
-                                            }
-
-                                            /* and now we add our parameter to the component we are dealing with */
-                                            component.Parameters.Add(key, comp);
-                                        }
-                                        else
-                                        {
-                                            throw new Exception("There is a BYML node in the parameters that isn't a BymlHashTable...");
-                                        }
+                                        continue;
                                     }
+
+                                    ComponentParam comp = new ComponentParam();
+                                    string key = ((BymlNode<string>)(ht["BBKey"])).Data;
+                                    comp.Type = type;
+
+                                    /* we look through the type that we set earlier to assign the proper initial value */
+                                    switch (type)
+                                    {
+                                        case "U32":
+                                            comp.InitValue = ((BymlNode<uint>)ht["InitVal"]).Data;
+                                            break;
+                                        /* S16 and S32 are both internally still a Int32 node, but S16 has bounds checks */
+                                        case "S16":
+                                        case "S32":
+                                            comp.InitValue = ((BymlNode<int>)ht["InitVal"]).Data;
+                                            break;
+                                        case "F32":
+                                            comp.InitValue = ((BymlNode<float>)ht["InitVal"]).Data;
+                                            break;
+                                        case "F64":
+                                            comp.InitValue = ((BymlNode<double>)ht["InitVal"]).Data;
+                                            break;
+                                        case "Bool":
+                                            comp.InitValue = ((BymlNode<bool>)ht["InitVal"]).Data;
+                                            break;
+                                    }
+
+                                    /* and now we add our parameter to the component we are dealing with */
+                                    component.Parameters.Add(key, comp);
+                                }
+                                else
+                                {
+                                    throw new Exception("There is a BYML node in the parameters that isn't a BymlHashTable...");
                                 }
                             }
                         }
-
-                        sComponents.Add(paramFileName, component);
                     }
+
+                    sComponents.Add(paramFileName, component);
                 }
 
                 sActors.Add(actorName, param);
