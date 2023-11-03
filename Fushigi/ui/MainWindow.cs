ffcaaaff745e1c28167aa2333f2166e94fdb183f
jupahe64 <jupahe@icloud.com>
2023-11-03 20:41:24 +0100

    hotfix: stop the gatekeeping

--- Fushigi/ui/MainWindow.cs
+++ Fushigi/ui/MainWindow.cs
@@ -42,7 +42,7 @@ namespace Fushigi.ui
                 RomFS.SetRoot(romFSPath);
             }
 
-            if (!ParamDB.sIsInit)
+            if (!ParamDB.sIsInit && !string.IsNullOrEmpty(RomFS.GetRoot()))
             {
                 ParamDB.Load();
             }
