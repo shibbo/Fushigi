d98eacdf2f4be182909ea7a3920277389ca49d46
shibbo <supershibbo12@gmail.com>
2023-11-06 14:29:47 -0500

    revert dictionary optimization

--- Fushigi/course/CourseActor.cs
+++ Fushigi/course/CourseActor.cs
@@ -209,24 +209,10 @@ namespace Fushigi.course
 
             if (mActorParameters.Count > 0)
             {
-                List<string> paramList = ParamDB.GetActorComponents(mActorName);
                 BymlHashTable dynamicNode = new();
 
                 foreach (KeyValuePair<string, object> dynParam in mActorParameters)
                 {
-                    ParamDB.ComponentParam? compParam = null;
-
-                    foreach (string p in paramList)
-                    {
-                        foreach (KeyValuePair<string, ParamDB.ComponentParam> kvp in ParamDB.GetComponentParams(p))
-                        {
-                            if (kvp.Key == dynParam.Key)
-                            {
-                                compParam = kvp.Value;
-                            }
-                        }
-                    }
-
                     object param = mActorParameters[dynParam.Key];
                     string shit = param.GetType().ToString();
                    
@@ -234,45 +220,26 @@ namespace Fushigi.course
                     switch (param.GetType().ToString())
                     {
                         case "System.UInt32":
-                            if ((uint)param != Convert.ToUInt32(compParam.Value.InitValue))
-                            {
                                 dynamicNode.AddNode(BymlNodeId.UInt, BymlUtil.CreateNode<uint>(dynParam.Key, (uint)param), dynParam.Key);
-                            }
                             break;
                         case "System.Int32":
-                            if ((int)param != Convert.ToInt32(compParam.Value.InitValue))
-                            {
                                 dynamicNode.AddNode(BymlNodeId.Int, BymlUtil.CreateNode<int>(dynParam.Key, (int)param), dynParam.Key);
-                            }
                             break;
                         case "System.Boolean":
-                            if ((bool)param != (bool)compParam.Value.InitValue)
-                            {
                                 dynamicNode.AddNode(BymlNodeId.Bool, BymlUtil.CreateNode<bool>(dynParam.Key, (bool)param), dynParam.Key);
-                            }
                             break;
                         case "System.String":
-                            if ((string)param != (string)compParam.Value.InitValue)
-                            {
                                 dynamicNode.AddNode(BymlNodeId.String, BymlUtil.CreateNode<string>(dynParam.Key, (string)param), dynParam.Key);
-                            }
                             break;
                         case "System.Single":
-                            if ((float)param != Convert.ToSingle(compParam.Value.InitValue))
-                            {
                                 dynamicNode.AddNode(BymlNodeId.Float, BymlUtil.CreateNode<float>(dynParam.Key, (float)param), dynParam.Key);
-                            }
                             break;
                         default:
                             break;
                     }
                 }
 
-                // did all of the nodes get stripped away due to all being default? if so, this node will be empty and thus we can discard
-                if (dynamicNode.Keys.Count > 0)
-                {
-                    table.AddNode(BymlNodeId.Hash, dynamicNode, "Dynamic");
-                }
+                table.AddNode(BymlNodeId.Hash, dynamicNode, "Dynamic");
             }
 
             if (mSystemParameters.Count > 0)
