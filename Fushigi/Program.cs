83ddaa508b83273e78492a27b9cc13445b0ec0fb
shibbo <supershibbo12@gmail.com>
2023-11-08 09:20:30 -0500

    version bump

--- Fushigi/Program.cs
+++ Fushigi/Program.cs
@@ -8,7 +8,7 @@ consoleWriter.AutoFlush = true;
 Console.SetOut(consoleWriter);
 Console.SetError(consoleWriter);
 
-Console.WriteLine("Starting Fushigi v0.2...");
+Console.WriteLine("Starting Fushigi v0.3...");
 Console.WriteLine("Loading user settings...");
 UserSettings.Load();
 Console.WriteLine("Loading parameter database...");
