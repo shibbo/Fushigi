4dceeaa39d016e59451420a96aaa2d2a107a6b5e
jupahe64 <jupahe@icloud.com>
2023-11-09 17:57:32 +0100

    begin work on Rail -> Tiles conversion

--- Fushigi/course/CourseUnit.cs
+++ Fushigi/course/CourseUnit.cs
@@ -3,15 +3,33 @@ using Fushigi.ui.widgets;
 using Fushigi.util;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using static Fushigi.course.CourseUnit;
+using Vector3 = System.Numerics.Vector3;
 
 namespace Fushigi.course
 {
     public class CourseUnit
     {
+        public enum ModelType
+        {
+            Solid,
+            SemiSolid,
+            NoCollision,
+            Bridge
+        }
+
+        public enum SkinDivision
+        {
+            FieldA,
+            FieldB
+        }
+
         public CourseUnit()
         {
             this.Walls = new List<Wall>();
@@ -22,8 +40,8 @@ namespace Fushigi.course
 
         public CourseUnit(BymlHashTable tbl)
         {
-            mModelType = BymlUtil.GetNodeData<int>(tbl["ModelType"]);
-            mSkinDivision = BymlUtil.GetNodeData<int>(tbl["SkinDivision"]);
+            mModelType = (ModelType)BymlUtil.GetNodeData<int>(tbl["ModelType"]);
+            mSkinDivision = (SkinDivision)BymlUtil.GetNodeData<int>(tbl["SkinDivision"]);
 
             if (tbl.ContainsKey("BeltRails"))
             {
@@ -87,8 +105,8 @@ namespace Fushigi.course
         public BymlHashTable BuildNode()
         {
             BymlHashTable table = new();
-            table.AddNode(BymlNodeId.Int, BymlUtil.CreateNode<int>("ModelType", mModelType), "ModelType");
-            table.AddNode(BymlNodeId.Int, BymlUtil.CreateNode<int>("SkinDivision", mSkinDivision), "SkinDivision");
+            table.AddNode(BymlNodeId.Int, BymlUtil.CreateNode<int>("ModelType", (int)mModelType), "ModelType");
+            table.AddNode(BymlNodeId.Int, BymlUtil.CreateNode<int>("SkinDivision", (int)mSkinDivision), "SkinDivision");
 
             BymlArrayNode beltsArray = new((uint)mBeltRails.Count);
 
@@ -169,19 +187,19 @@ namespace Fushigi.course
         public struct Rail
         {
             public bool IsClosed;
-            public List<System.Numerics.Vector3?> mPoints;
+            public List<Vector3?> mPoints;
             public bool IsInternal;
 
             public Rail()
             {
                 IsInternal = false;
                 IsClosed = true;
-                mPoints = new List<System.Numerics.Vector3?>();
+                mPoints = new List<Vector3?>();
             }
         }
 
-        public int mModelType;
-        public int mSkinDivision;
+        public ModelType mModelType;
+        public SkinDivision mSkinDivision;
 
         //Editor render objects
         internal List<Wall> Walls = new List<Wall>();
@@ -199,6 +217,171 @@ namespace Fushigi.course
         internal Wall(CourseUnit unit)
         {
             ExternalRail = new BGUnitRail(unit, new CourseUnit.Rail());
+        }
+    }
+
+    public class TilesComponent
+    {
+        public enum SlopeType
+        {
+            UpperLeft,
+            UpperRight,
+            LowerLeft,
+            LowerRight,
+        }
+
+        public Vector3 mOrigin;
+        public readonly InfiniteTileMap mTileMap = new();
+        public readonly List<(int width, int height, SlopeType type)> mSlopes = [];
+
+        public static TilesComponent CreateFromWall(Wall wall)
+        {
+            TilesComponent component = new TilesComponent();
+
+            HashSet<(int x, int y)> blockedTiles = [];
+
+            Vector2[] externPoints2D = new Vector2[wall.ExternalRail.Points.Count];
+            Vector2[][] internPoints2D = new Vector2[wall.InternalRails.Count][];
+
+            var min = new Vector3(float.PositiveInfinity);
+            var max = new Vector3(float.NegativeInfinity);
+
+            for (int i = 0; i < wall.ExternalRail.Points.Count; i++)
+            {
+                BGUnitRail.RailPoint? point = wall.ExternalRail.Points[i];
+                externPoints2D[i] = new Vector2(point.Position.X, point.Position.Y);
+                min.X = MathF.Min(min.X, point.Position.X);
+                min.Y = MathF.Min(min.Y, point.Position.Y);
+                min.Z = MathF.Min(min.Z, point.Position.Z);
+                max.X = MathF.Max(max.X, point.Position.X);
+                max.Y = MathF.Max(max.Y, point.Position.Y);
+                max.Z = MathF.Max(max.Z, point.Position.Z);
+            }
+
+            for (int iInternalRail = 0; iInternalRail < wall.InternalRails.Count; iInternalRail++)
+            {
+                var internalRail = wall.InternalRails[iInternalRail];
+                internPoints2D[iInternalRail] = new Vector2[internalRail.Points.Count];
+
+                for (int i = 0; i < internalRail.Points.Count; i++)
+                {
+                    BGUnitRail.RailPoint? point = internalRail.Points[i];
+                    var point2D = new Vector2(point.Position.X, point.Position.Y);
+                    internPoints2D[iInternalRail][i] = point2D;
+                    min.X = MathF.Min(min.X, point.Position.X);
+                    min.Y = MathF.Min(min.Y, point.Position.Y);
+                    min.Z = MathF.Min(min.Z, point.Position.Z);
+                    max.X = MathF.Max(max.X, point.Position.X);
+                    max.Y = MathF.Max(max.Y, point.Position.Y);
+                    max.Z = MathF.Max(max.Z, point.Position.Z);
+                }
+            }
+
+            var size = max - min;
+            var size2D = new Vector2(size.X, size.Y);
+            component.mOrigin = min;
+            var origin2D = new Vector2(min.X, min.Y);
+
+            #region Slopes
+            for (int iSegment = 0; iSegment < externPoints2D.Length; iSegment++)
+            {
+                var p0 = externPoints2D[iSegment];
+                var p1 = externPoints2D[(iSegment + 1) % externPoints2D.Length];
+
+                if (
+                    MathF.IEEERemainder(p0.X - min.X, 1) != 0 ||
+                    MathF.IEEERemainder(p0.Y - min.Y, 1) != 0 ||
+                    MathF.IEEERemainder(p1.X - min.X, 1) != 0 ||
+                    MathF.IEEERemainder(p1.Y - min.Y, 1) != 0
+                    )
+                {
+                    //slope cannot be placed off (tile)grid
+                    continue;
+                }
+
+                var slope = Math.Abs((p1.Y - p0.Y) / (p1.X - p0.X));
+
+                if (slope is not (1 or 0.5f))
+                    //slope is not supported by the game
+                    continue;
+
+                SlopeType slopeType = default;
+                if (p0.X < p1.X && p0.Y < p1.Y)
+                    slopeType = SlopeType.LowerRight;
+                else if (p0.X < p1.X && p0.Y > p1.Y)
+                    slopeType = SlopeType.LowerLeft;
+                else if (p0.X > p1.X && p0.Y < p1.Y)
+                    slopeType = SlopeType.UpperRight;
+                else if (p0.X > p1.X && p0.Y > p1.Y)
+                    slopeType = SlopeType.UpperLeft;
+
+                int slopeWidth = (int)Math.Min(slope, 1);
+                int slopeHeight = (int)slope;
+
+                int slopeCount = (int)Math.Abs(p1.X - p0.X) / slopeWidth;
+
+                for (int iSlope = 0; iSlope < slopeCount; iSlope++)
+                {
+                    var triP0 = p0 + (p1 - p0) * iSlope / slopeCount - origin2D;
+                    var triP1 = p0 + (p1 - p0) * (iSlope + 1) / slopeCount - origin2D;
+                    var triP2 =
+                        (slopeType is SlopeType.LowerRight or SlopeType.UpperLeft) ?
+                        new Vector2(triP0.X, triP1.Y) :
+                        new Vector2(triP1.X, triP0.Y);
+
+                    int tileX = Math.Min((int)triP0.X, (int)triP1.X);
+                    int tileY = Math.Min((int)triP0.Y, (int)triP1.Y);
+
+                    blockedTiles.Add((tileX, tileY));
+
+                    if (slope == 0.5f)
+                        blockedTiles.Add((tileX + 1, tileY));
+
+                    component.mSlopes.Add((slopeWidth, slopeHeight, slopeType));
+                }
+            }
+            #endregion
+
+            int? FillFunction((int x, int y) tilePos)
+            {
+                var (x, y) = tilePos;
+
+                int windingNum;
+                if (blockedTiles.Contains((x, y)))
+                    return null;
+
+                bool isHole = false;
+
+                for (int iInternalRail = 0; iInternalRail < wall.InternalRails.Count; iInternalRail++)
+                {
+                    Array.Reverse(internPoints2D[iInternalRail]);
+
+                    windingNum = MathUtil.PolygonWindingNumber(
+                    origin2D + new Vector2(x, y) + new Vector2(0.5f), internPoints2D[iInternalRail]);
+
+                    Array.Reverse(internPoints2D[iInternalRail]);
+
+                    if (windingNum != 0)
+                    {
+                        isHole = true;
+                        break;
+                    }
+                }
+
+                if (isHole)
+                    return null;
+
+
+                windingNum = MathUtil.PolygonWindingNumber(
+                    origin2D + new Vector2(x, y) + new Vector2(0.5f), externPoints2D);
+                bool isInside = windingNum != 0;
+
+                return isInside ? 0 : null;
+            }
+
+            component.mTileMap.FillTiles(FillFunction, Vector2.Zero, size2D);
+
+            return component;
         }
     }
 
