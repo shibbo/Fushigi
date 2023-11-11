7093ba73600114c8bd2ce5b73f3c1fd5af8a5d98
KillzXGaming <millernathan35@gmail.com>
2023-11-11 11:27:07 -0500

    Make the default value type an object.

--- Fushigi.Byml/Serializer/BymlProperty.cs
+++ Fushigi.Byml/Serializer/BymlProperty.cs
@@ -9,7 +9,7 @@ namespace Fushigi.Byml.Serializer
     public class BymlProperty : Attribute
     {
         public string Key { get; set; }
-        public dynamic DefaultValue { get; set; }
+        public object DefaultValue { get; set; }
 
         public bool Optional = true;
 
