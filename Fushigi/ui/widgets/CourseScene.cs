5bba792daa11cc3bbd4322702ba2ae545db09b94
KillzXGaming <millernathan35@gmail.com>
2023-11-19 10:00:54 -0500

    Ensure to remove unused actor from cache to prevent possible gl crashes

--- Fushigi/ui/widgets/CourseScene.cs
+++ Fushigi/ui/widgets/CourseScene.cs
@@ -120,15 +120,23 @@ namespace Fushigi.ui.widgets
             //All resource files to load
             resourceFiles = resourceFiles.Distinct().Where(x => !string.IsNullOrEmpty(x)).ToList();
             //Unload any unused resources in the cache
+
+            List<string> removed = new List<string>();
             foreach (var bfres in BfresCache.Cache)
             {
                 //Not currently used by area, dispose
                 if (!resourceFiles.Contains(bfres.Key))
                 {
                     bfres.Value.Dispose();
+                    removed.Add(bfres.Key);
+
                     Console.WriteLine($"Disposing resource {bfres.Key}");
                 }
             }
+
+            foreach (var bfres in removed)
+                BfresCache.Cache.Remove(bfres);
+
             //Load all used resources
             foreach (var file in resourceFiles)
                 BfresCache.Load(gl, file);
