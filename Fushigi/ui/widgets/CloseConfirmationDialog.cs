9cb85d2ab3089510641e5619eaedcfe521123f29
jupahe64 <jupahe@icoud.com>
2023-12-01 03:58:28 +0100

    switch to a better popup modal system fixes a bug where the window would become unusable in certain scenarios

--- Fushigi/ui/widgets/CloseConfirmationDialog.cs
+++ Fushigi/ui/widgets/CloseConfirmationDialog.cs
@@ -1,57 +1,43 @@
-﻿using Fushigi.param;
+﻿using Fushigi.ui.modal;
+using Fushigi.util;
 using ImGuiNET;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fushigi.ui.widgets
 {
-    class CloseConfirmationDialog
+
+    class CloseConfirmationDialog : IPopupModal<CloseConfirmationDialog.DialogResult>
     {
-        public enum Result
+        public enum DialogResult
         {
             Yes,
             No
         }
 
-        public static bool Draw(bool doShow, [MaybeNullWhen(false)] out Result? result)
+        public static async Task<DialogResult> ShowDialog(IPopupModalHost modalHost)
         {
-            result = null;
 
-            Vector2 center = ImGui.GetMainViewport().GetCenter();
-            ImGui.SetNextWindowPos(center, ImGuiCond.Appearing, new Vector2(0.5f, 0.5f));
+            var result = await modalHost.ShowPopUp(new CloseConfirmationDialog(), "Unsaved changes.",
+                ImGuiWindowFlags.AlwaysAutoResize);
 
-            if (doShow && !ImGui.IsPopupOpen("CloseConfirmation"))
-                ImGui.OpenPopup("CloseConfirmation");
+            if (result.wasClosed)
+                return DialogResult.No;
 
-            if (ImGui.BeginPopupModal("CloseConfirmation", ref doShow, ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoDecoration))
-            {
-                float centerXText = (ImGui.GetWindowWidth() - ImGui.CalcTextSize("Unsaved changes.").X) * 0.5f;
-                ImGui.SetCursorPosX(centerXText);
-
-                ImGui.Text("Unsaved changes.");
-                ImGui.NewLine();
-                ImGui.Text("Do you still want to close?");
-                ImGui.NewLine();
-
-                float centerXButtons = (ImGui.GetWindowWidth() - ImGui.CalcTextSize("Yes No").X) * 0.4f;
-                ImGui.SetCursorPosX(centerXButtons);
-                if (ImGui.Button("Yes"))
-                    result = Result.Yes;
-             
-                ImGui.SameLine();
-                if (ImGui.Button("No"))
-                    result = Result.No;
-
-                ImGui.EndPopup();
-            }
-
-            return result != null;
+            return result.result;
         }
 
+        public void DrawModalContent(Promise<DialogResult> promise)
+        {
+            ImGui.Text("Do you still want to close?");
+            ImGui.NewLine();
+
+            float centerXButtons = (ImGui.GetWindowWidth() - ImGui.CalcTextSize("Yes No").X) * 0.4f;
+            ImGui.SetCursorPosX(centerXButtons);
+            if (ImGui.Button("Yes"))
+                promise.SetResult(DialogResult.Yes);
+
+            ImGui.SameLine();
+            if (ImGui.Button("No"))
+                promise.SetResult(DialogResult.No);
+        }
     }
 }
---
+++ Fushigi/util/Promise.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Fushigi.util
+{
+    public class Promise<T>
+    {
+        private T? mValue;
+        private bool mHasResult = false;
+        public void SetResult(T value)
+        {
+            mValue = value;
+            mHasResult = true;
+        }
+
+        public bool TryGetResult([NotNullWhen(true)] out T? result)
+        {
+            result = mValue;
+            return mHasResult;
+        }
+    }
+}
